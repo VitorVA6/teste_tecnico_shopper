55dd75628cabede21e3e38bebebf0f0dc449d299
Vitor Vaz Andrade <vitorandrade@ecomp.uefs.br>
2023-09-07 09:37:17 -0300

    feat: create the react app

--- api/controller/ProductController.js
+++ api/controller/ProductController.js
@@ -6,4 +6,8 @@ module.exports = class ProductController{
         const products = await Product.getById(18)
         return res.status(200).json(products)
     }
+
+    static async update(req, res){
+        res.status(200).json({message: 'Produtos atualizados com sucesso'})
+    }
 }
